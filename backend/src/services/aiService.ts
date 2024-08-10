cd318fbf1ac4d8ec216634d22c4d1ed0811c867b
amantank <heyamantank@outlook.com>
2024-08-10 17:39:00 +0530

    Aiservice

--- backend/src/services/aiService.ts
+++ backend/src/services/aiService.ts
@@ -1,6 +1,5 @@
 // src/services/aiService.ts
 // src/services/aiService.ts
-import axios from 'axios';
 
 const HUGGINGFACE_API_URL =
   "https://api-inference.huggingface.co/models/unsloth/Meta-Llama-3.1-8B-bnb-4bit";
@@ -10,26 +9,19 @@ export const generateResponse = async (
   question: string,
   code: string,
   userExplanation: string,
-  language: string,
+  language: string
 ): Promise<string> => {
   const prompt = `Role: Interviewer\nGiven the data about coding question, comment on the user's provided approach with respect to the provided solution.\n${question}`;
   try {
-    const response = await axios.post(
-      HUGGINGFACE_API_URL,
-      {
-        inputs: prompt,
-        parameters: {
-          max_new_tokens: 200,
-        },
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${HUGGINGFACE_API_KEY}`,
-        },
-      }
-    );
-    console.log(response.data);
-    return response.data.generated_text.trim();
+    console.log(question, code, userExplanation, language);
+    let response: string = "Hard coded";
+    // const response = await axios.post(
+    //   HUGGINGFACE_API_URL,
+    //   {
+    //   }
+    // // );
+    // console.log(response.data);
+    return response;
   } catch (error) {
     console.error("Error generating response:", error);
     throw error;
