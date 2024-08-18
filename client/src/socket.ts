a5e5ba2c7836e8d8a20cd54db4837e3a2dcf3422
Priyansh Jhalora <79319855+Priyansh-Breach@users.noreply.github.com>
2024-08-18 15:08:23 +0530

    socket commit

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,17 +1,10 @@
+// frontend/src/socket.ts
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("http://localhost:3000");
 
 socket.on("connect", () => {
   console.log("Connected to WebSocket server");
-
-  // Example of emitting an event to the server
-  socket.emit("exampleEvent", { message: "Hello from the client!" });
-
-  // Example of listening to a response event from the server
-  socket.on("responseEvent", (data) => {
-    console.log("Received from server:", data);
-  });
 });
 
 socket.on("disconnect", () => {
