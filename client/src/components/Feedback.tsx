b48b4113c701fcc12860fabd663b15c5551c8aaa
amantank <heyamantank@outlook.com>
2024-09-05 18:20:27 +0530

    Fixes

--- client/src/components/Feedback.tsx
+++ client/src/components/Feedback.tsx
@@ -98,7 +98,7 @@ export const FeedbackForm = () => {
       <DropdownMenu>
         <DropdownMenuTrigger>
           <div className="sm:flex sm:justify-center">
-            <div className="relative shadow-gradient-green rounded-full px-3 text-sm leading-6 ring-1 ring-gray-900 hover:ring-gray-900/20">
+            <div className="relative  rounded-full px-3 text-sm leading-6 ring-1 ring-green-900 hover:ring-green-900/20">
               <a
                 href="#"
                 className="text-foreground font-light transition-colors hover:text-foreground"
@@ -108,8 +108,8 @@ export const FeedbackForm = () => {
             </div>
           </div>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className="m-4 ml-16 rounded-xl">
-          <Card className="w-sm max-w-md p-6 grid gap-6">
+        <DropdownMenuContent className="m-4 ml-16 border-none ring-1 transition ring-green-900 hover:ring-green-900/20 rounded-xl">
+          <Card className="w-sm max-w-md p-6 grid gap-6 border-none  ">
             <div className="grid gap-2">
               <CardTitle className="text-xl font-semibold">Feedback</CardTitle>
               <CardDescription className="text-muted-foreground">
